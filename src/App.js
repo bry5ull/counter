bd5f40cf60325dd37e70451dc04f4511017517b8
bry5ull <bry@bry5ull.dev>
2022-06-16 20:31:55 -0500

    test3

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import { useState } from 'react'
+import { ten, addTen } from './utils/utils'
 
 function App(){
-    let [ counter, changeCounter ] = useState(1)
-    window.changeCounter = changeCounter
+    let [ counter, setCounter ] = useState(1)
+    const [user, setUser] = useState(null)
+    const [username, setUsername] = useState('')
+
+    const handleChange = (e) => {
+      setUsername(e.target.value)
+    }
     return (
       <div>
         <h1>{counter}</h1>
+        <h2>{ten}</h2>
+        <h3>{addTen(15)}</h3>
         <h4>{counter}</h4>
-        </div>
+      </div>
     )
 }
 
